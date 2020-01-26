12b05f44175556777a884773dac6d8880cf33d18
ilyasa Azmi <theazmi99ilyasa@gmail.com>
2020-01-26 08:08:27 +0700

    Initial Commit

---
+++ TesMujigae/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TesMujigae
+//
+//  Created by Ilyasa Azmi on 26/01/20.
+//  Copyright © 2020 Ilyasa Azmi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
